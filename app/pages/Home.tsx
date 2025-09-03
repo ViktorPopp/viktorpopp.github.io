9a9a442ade7b3eb35385caac50495a11cacfdca5
Viktor Popp <viktorpopp.dev@gmail.com>
2025-09-03 11:52:15 +0200

    Deleted old favicon

--- app/pages/Home.tsx
+++ app/pages/Home.tsx
@@ -4,7 +4,7 @@ import Icon from "../components/Icon";
 export default function Home() {
   return (
     <div className="flex items-center justify-center h-full px-4">
-      <div className="max-w-4xl mx-auto text-center">
+      <div className="max-w-4xl mx-auto text-center ">
         <div className="mb-8">
           <Icon />
 
